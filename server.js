59839dcf9eeb1e596ad73b3e2521c245a5e3d6f8
ROB1N <ka@madrobots.ru>
2017-12-01 15:04:38 +0700

    [master] try to deal with Error Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist/index.html'))
 });
